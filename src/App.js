142b8e0bb40713207ca16543c736c68793fae48a
dream <dreamka1991@gmail.com>
2025-08-06 21:11:00 +0300

    стартовый коммит

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import ArticleList from './components/ArticleList';
+import ArticlePage from './components/ArticlePage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<ArticleList />} />
+            <Route path="/article/:id" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
